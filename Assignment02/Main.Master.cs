65443011b607fe6893a7d29d6f93ead5914f4e14
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-11-17 12:27:41 -0500

    Final Assignment02

---
+++ Assignment02/Main.Master.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment02
+{
+    public partial class Main : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SetActivePage();
+        }
+
+        protected void SetActivePage()
+        {
+            switch (Page.Title)
+            {
+                case "Default":
+                    chandra.Attributes.Add("Class", "active");
+                    break;
+                case "Narayan":
+                    narayan.Attributes.Add("Class", "active");
+                    break;
+                case "Bishnu":
+                    bishnu.Attributes.Add("Class", "active");
+                    break;
+                case "Balkrishna":
+                    balkrishna.Attributes.Add("Class", "active");
+                    break;
+                case "Fred":
+                    fred.Attributes.Add("Class", "active");
+                    break;
+            }
+        }
+    }
+}
